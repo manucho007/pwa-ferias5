c24250550efb090be3ca7d41fa7334eb8afa7a7b
Manuel Rodriguez <manrodri92@gmail.com>
2017-11-21 01:48:09 -0400

    Added Admin guard

---
+++ src/app/core/auth-admin.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot,Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { AuthService} from './auth.service';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
+
+@Injectable()
+export class AuthAdminGuard implements CanActivate {
+  constructor(private auth:AuthService,
+              private router: Router){
+
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.auth.user
+      .take(1)
+      // .map(user=>!!user)
+      // With condition
+      .map(user=>!!(user && user.isAdmin == true))
+      .do(loggedIn=>{
+        if(!loggedIn){
+          console.log("Access denied")
+          this.router.navigate(['/login']);
+        }
+      })
+  }
+}
